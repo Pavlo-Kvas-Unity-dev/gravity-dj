9288fcaf04c406210929797811f47082c6df0e98
Pavlo Kvas <kvaspasha@gmail.com>
2019-02-12 19:38:36 +0200

    spawn agents at random locations

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject FlyingAgentPrefab;
+
+    public float SpawnRadius;
+
+    public Transform SpawnCenter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        var spawnPos = (Vector2)SpawnCenter.position + Random.insideUnitCircle * SpawnRadius;
+
+        Instantiate(FlyingAgentPrefab, spawnPos, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Spawn();
+        }
+    }
+}

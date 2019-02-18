a76f2988a89fdf8bdb444d31f285743a1dba3cb5
Pavlo Kvas <kvaspasha@gmail.com>
2019-02-18 10:14:16 +0200

    Destroy agent when they flew off the screen; only one agent at a time

---
+++ Assets/Scripts/FlyingAgent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingAgent : MonoBehaviour
+{
+    public static int NumAgents { get; private set; } = 0;
+
+    void Awake()
+    {
+        NumAgents++;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        NumAgents--;
+        GameObject.FindObjectOfType<Spawner>().Spawn(); //todo
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,15 +10,26 @@ public class Spawner : MonoBehaviour
 
     public Transform SpawnCenter;
     public int InitialSpeed = 5;
+    public bool SpawnOnStart = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        Spawn();
+        if (SpawnOnStart)
+        {
+            Spawn();
+        }
     }
 
-    private void Spawn()
+    public void Spawn()
     {
+        if (FlyingAgent.NumAgents > 0)
+        {
+            Debug.Log("agent is already on screen");
+            return;
+        }
+
+        //spawn only if no Agent is on the screen
         var spawnPos = (Vector2)SpawnCenter.position + Random.insideUnitCircle * SpawnRadius;
 
         var movement = Instantiate(FlyingAgentPrefab, spawnPos, Quaternion.identity).GetComponent<Movement>();

d15a138cd96379ed28d87bd4c99e35646eca268e
Pavlo Kvas <kvaspasha@gmail.com>
2019-02-22 18:18:58 +0200

    spawn balls outside of the circle and insibe boundaries; balls spawn motionless

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject FlyingAgentPrefab;
 
-    public float SpawnRadius;
+    public float FieldSize = 5;
+    private float FieldHalfSize => FieldSize / 2;
+
+    public float MaxDistanceFromBoundary = 1;
 
     public Transform SpawnCenter;
     public int InitialSpeed = 5;
     public bool SpawnOnStart = true;
 
+
     // Start is called before the first frame update
+
     void Start()
     {
         if (SpawnOnStart)
@@ -30,12 +38,36 @@ public class Spawner : MonoBehaviour
     {
         yield return null;
 
-        //spawn only if no Agent is on the screen
-        var spawnPos = (Vector2) SpawnCenter.position + Random.insideUnitCircle * SpawnRadius;
+        SpawnImmediate();
+        yield break;
+    }
 
+    private void SpawnImmediate()
+    {
+        float objectRadius = FlyingAgentPrefab.GetComponentInChildren<CircleCollider2D>().radius;
+        Assert.IsTrue(MaxDistanceFromBoundary > objectRadius);
+
+        var xPos = Random.Range(0, FieldSize);
+        var yPos = Random.Range(0, FieldSize);
+
+        CheckBoundaries(ref xPos, objectRadius, FieldSize);
+        CheckBoundaries(ref yPos, objectRadius, FieldSize);
+
+        var spawnPos = new Vector2(xPos, yPos);
+        spawnPos -= Vector2.one * FieldHalfSize;
+        float minAllowedDistanceFromCenter = FieldHalfSize - MaxDistanceFromBoundary;
+        float cappedMagnitude = Mathf.Max(spawnPos.magnitude, minAllowedDistanceFromCenter);
+        spawnPos = spawnPos.normalized * cappedMagnitude;
+
+        //spawn only if no Agent is on the screen
         var movement = Instantiate(FlyingAgentPrefab, spawnPos, Quaternion.identity).GetComponent<Movement>();
         movement.Init(InitialSpeed);
-        yield break;
+    }
+
+    private void CheckBoundaries(ref float coord, float objectRadius, float fieldSize)
+    {
+        coord = Mathf.Max(coord, objectRadius);
+        coord = Mathf.Min(coord, fieldSize - objectRadius);
     }
 
     void Update()

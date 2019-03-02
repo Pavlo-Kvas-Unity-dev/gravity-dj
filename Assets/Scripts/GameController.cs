2f50a0b00c9c3c90c9aab2467643b4d54cba5dc0
Pavlo Kvas <kvaspasha@gmail.com>
2019-03-02 15:55:30 +0200

    add countdown

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
-
+    [SerializeField] private TextMeshProUGUI countdownText;
     private int score = 0;
-    
+    private bool isGamePlaing = false;
+    private float timeLeft;
+    [SerializeField] private float gameDuration = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
         UpdateScoreUI();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        timeLeft = gameDuration;
+        isGamePlaing = true;
     }
 
     public void OnAgentFlewAway(FlyingAgent flyingAgent)
@@ -24,5 +35,16 @@ public class GameController : MonoBehaviour
     private void UpdateScoreUI()
     {
         scoreText.text = $"Score: {score}";
+    }
+
+    private void FixedUpdate()
+    {
+        if (isGamePlaing)
+        {
+            countdownText.text = $"Time left: {(int) timeLeft}";
+            timeLeft -= Time.deltaTime;
+            
+            if (timeLeft < 0) isGamePlaing = false;
+        }
     }
 }

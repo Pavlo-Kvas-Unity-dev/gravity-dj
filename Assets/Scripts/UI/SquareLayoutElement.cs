907619b8a8f1e8b9762951aa653c62b81ec2edb8
Pavlo Kvas <kvaspasha@gmail.com>
2019-03-07 18:18:33 +0200

    add side panels that recalculate its size to not overlap with game field

---
+++ Assets/Scripts/UI/SquareLayoutElement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SquareLayoutElement : MonoBehaviour, ILayoutElement
+{
+    
+
+
+    public void CalculateLayoutInputHorizontal()
+    {
+        var parentRectTransform = transform.parent as RectTransform;
+        minWidth = preferredWidth = parentRectTransform.rect.height;
+        flexibleWidth = 0f;
+    }
+
+    public void CalculateLayoutInputVertical()
+    {
+         
+    }
+
+    public float minWidth { get; set; }
+    public float preferredWidth { get; set; }
+    public float flexibleWidth { get; set; }
+    public float minHeight { get; }
+    public float preferredHeight { get; }
+    public float flexibleHeight { get; }
+    public int layoutPriority { get; }
+}

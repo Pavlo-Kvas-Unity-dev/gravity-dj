a67f1971009470571f1e244f8c63e2fa8a7f23c1
Pavlo Kvas <kvaspasha@gmail.com>
2019-03-07 10:10:01 +0200

    refactor Spawner class

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -34,7 +34,10 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameController gameController;
 
     // Start is called before the first frame update
-
+    void Awake()
+    {
+        Assert.IsTrue(MaxDistanceFromBoundary > ObjectRadius);
+    }
 
     void Start()
     {
@@ -57,29 +60,26 @@ public class Spawner : MonoBehaviour
         Gizmos.color = prevColor;
     }
 
-
     public void Spawn()
     {
-        Assert.IsTrue(MaxDistanceFromBoundary > ObjectRadius);
-
         float spawnableFieldSize = fieldController.FieldSize - 2 * fieldController.BorderSize;
-        
-        var xPos = Random.Range(0f, spawnableFieldSize);
-        var yPos = Random.Range(0f, spawnableFieldSize);
 
-        //for test 
-        CheckBoundaries(ref xPos, ObjectRadius, spawnableFieldSize);
-        CheckBoundaries(ref yPos, ObjectRadius, spawnableFieldSize);
+        var xPos = RandomCoordInsideBoundaries();
+        var yPos = RandomCoordInsideBoundaries();
 
         var spawnPos = new Vector2(xPos, yPos);
-        
+
         spawnPos -= Vector2.one * spawnableFieldSize/2;
+
         float cappedMagnitude = Mathf.Max(spawnPos.magnitude, MinAllowedDistanceFromCenter(ObjectRadius) + ObjectRadius);
+
         spawnPos = spawnPos.normalized * cappedMagnitude;
 
         //spawn only if no Agent is on the screen
         var flyingAgentGO = Instantiate(FlyingAgentPrefab, spawnPos, Quaternion.identity);
+
         var movement = flyingAgentGO.GetComponent<Movement>();
+
         var flyingAgent = flyingAgentGO.GetComponent<FlyingAgent>();
 
         flyingAgent.flyAway += new FlyingAgent.OnAgentFlyThroughHoleEventHandler(sender =>
@@ -87,18 +87,19 @@ public class Spawner : MonoBehaviour
             gameController.OnAgentFlewAway(sender);
             Spawn();
         });
+
         movement.Init(InitialSpeed);
+
+        float RandomCoordInsideBoundaries()
+        {
+            float coord = Random.Range(0f, spawnableFieldSize);
+            return Mathf.Clamp(coord, ObjectRadius, spawnableFieldSize - ObjectRadius);
+        }
     }
 
     private float MinAllowedDistanceFromCenter(float objectRadius)
     {
         return fieldController.FieldSize/2 - fieldController.BorderSize - MaxDistanceFromBoundary + objectRadius;
-    }
-
-    private void CheckBoundaries(ref float coord, float objectRadius, float fieldSize)
-    {
-        coord = Mathf.Max(coord, objectRadius);
-        coord = Mathf.Min(coord, fieldSize - objectRadius);
     }
 
     void Update()

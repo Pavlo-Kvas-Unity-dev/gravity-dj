0cf4d24386b351211ef28d44bbccdc35d6d67765
Pavlo Kvas <kvaspasha@gmail.com>
2019-03-19 14:24:25 +0200

    update localization

--- Assets/InfinityEngine/Gen/Scripts/R3.cs
+++ Assets/InfinityEngine/Gen/Scripts/R3.cs
@@ -4,19 +4,25 @@ namespace InfinityEngine.Localization {
 /// <summary>This class is generated automaticaly by InfinityEngine, it contains constants used by many scripts.  DO NOT EDIT IT !</summary>
 	public static class R3 {
 		public static class strings {
-			public const string Names = "HelloWorld";
+			public const string Names = "EnglishLanguageButton,UkrainianLanguageButton,RussianLanguageButton,MenuButton,PlayAgainButton,SettingsButton,HelpButton,ExitButton,ReturnButton";
 
-			public static ISIString HelloWorld = new ISIString("HelloWorld");
+			public static ISIString EnglishLanguageButton = new ISIString("EnglishLanguageButton");
+			public static ISIString UkrainianLanguageButton = new ISIString("UkrainianLanguageButton");
+			public static ISIString RussianLanguageButton = new ISIString("RussianLanguageButton");
+			public static ISIString MenuButton = new ISIString("MenuButton");
+			public static ISIString PlayAgainButton = new ISIString("PlayAgainButton");
+			public static ISIString SettingsButton = new ISIString("SettingsButton");
+			public static ISIString HelpButton = new ISIString("HelpButton");
+			public static ISIString ExitButton = new ISIString("ExitButton");
+			public static ISIString ReturnButton = new ISIString("ReturnButton");
 		}
 		public static class audios {
-			public const string Names = "Hello";
+			public const string Names = "";
 
-			public static AudioClip Hello => ISILocalization.GetAudio("Hello");
 		}
 		public static class sprites {
-			public const string Names = "LocalizedSprite";
+			public const string Names = "";
 
-			public static Sprite LocalizedSprite => ISILocalization.GetSprite("LocalizedSprite");
 		}
 	}
 }

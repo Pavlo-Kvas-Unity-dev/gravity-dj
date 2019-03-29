baa26978541229c071e3c7fce0ee1684ea4adf6e
Pavlo Kvas <kvaspasha@gmail.com>
2019-03-29 17:55:33 +0200

    refactor SquareFitInParentLayoutElement

---
+++ Assets/Scripts/UI/SquareFitInParentLayoutElement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GravityDJ.UI
+{
+    
+    /// <summary>
+    /// This layout element sets width and height to be equal to the height of the target transform
+    /// </summary>
+    public class SquareFitInParentLayoutElement : MonoBehaviour, ILayoutElement
+    {
+        [Tooltip("This layout element sets width and height to be equal to the height of the target transform")]
+        [SerializeField] private RectTransform targetHeightTransform;
+        
+        public void CalculateLayoutInputHorizontal()
+        {
+            minHeight = preferredHeight = minWidth = preferredWidth = targetHeightTransform.rect.height;
+            
+            flexibleWidth = 0f;
+        }
+
+        public void CalculateLayoutInputVertical()
+        {
+        }
+
+        public float minWidth { get; set; }
+        public float preferredWidth { get; set; }
+        public float flexibleWidth { get; set; }
+        public float minHeight { get; set; }
+        public float preferredHeight { get; set; }
+        public float flexibleHeight { get; }
+        public int layoutPriority { get; }
+    }
+}

edbd83fde1ed0d4c0849b530b9db3dc9198b8271
Pavlo Kvas <kvaspasha@gmail.com>
2019-03-30 10:54:44 +0200

    set localization to systems language closes #48

---
+++ Assets/Scripts/LocalizationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using InfinityEngine.Localization;
+using UnityEngine;
+
+public class LocalizationController : MonoBehaviour
+{
+    [SerializeField] private List<LangFlagPair> supportedLanguages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TrySetSystemLanguage();
+    }
+
+    private void TrySetSystemLanguage()
+    {
+        var curSystemLangFlagPair = supportedLanguages.First(x => x.SystemLanguage == Application.systemLanguage);
+        
+        if (curSystemLangFlagPair == null) return;
+        
+        var systemLang = curSystemLangFlagPair.ISIlanguage;
+
+        if (ISILocalization.CurrentLanguage == systemLang) return;
+
+        var localizedLanguage = ISILocalization.Instance.LocalizedLanguages.First(model => model.Language == systemLang);
+        if (localizedLanguage != null)
+        {
+            ISILocalization.ChangeLanguage(systemLang);
+        }
+    }
+
+    [Serializable]
+    class LangFlagPair
+    {
+        public SystemLanguage SystemLanguage;
+        public Language ISIlanguage;
+    }
+}

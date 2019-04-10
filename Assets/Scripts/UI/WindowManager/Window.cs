14620a5003857d26edf09bc15c338d26c712f0f9
Pavlo Kvas <kvaspasha@gmail.com>
2019-04-10 19:52:37 +0300

    add Window and IWindowManager scripts; rename folder

---
+++ Assets/Scripts/UI/WindowManager/Window.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Window
+{
+    #region Properties
+
+    /// <summary>
+    ///   Data the window needs, e.g. for initialization.
+    /// </summary>
+    public object Context { get; set; }
+
+    /// <summary>
+    ///   Indicates if the window is loaded.
+    /// </summary>
+    public bool Loaded { get; set; }
+
+    /// <summary>
+    ///   Callback when window was closed.
+    ///   Parameter 1: Closed window.
+    ///   Parameter 2: Return value of window.
+    /// </summary>
+    public Action<Window, object> OnClosed { get; set; }
+
+    /// <summary>
+    ///   Callback when window was opened.
+    ///   Parameter 1: Opened window.
+    /// </summary>
+    public Action<Window> OnOpened { get; set; }
+
+    /// <summary>
+    ///   Root transforms of loaded window scene.
+    /// </summary>
+    public Transform[] Roots { get; set; }
+
+    /// <summary>
+    ///   Scene the window was loaded from.
+    /// </summary>
+    public Scene Scene { get; set; }
+
+    /// <summary>
+    ///   Unique id of window.
+    /// </summary>
+    public string WindowId { get; set; }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    public void Hide()//todo
+    {
+        foreach (var root in this.Roots)
+        {
+            root.gameObject.SetActive(false);
+        }
+    }
+
+    public void Show()//todo
+    {
+        foreach (var root in this.Roots)
+        {
+            root.gameObject.SetActive(true);
+        }
+    }
+
+    #endregion
+}
